587580adddfc66d5c2ebe948a8c582bef6cd4778
Heber Lazo <heberlazo@gmail.com>
2024-10-24 15:42:24 -0500

    Added PDF report

---
+++ src/main/java/com/cocotera/controller/ReportesController.java
@@ -0,0 +1,63 @@
+package com.cocotera.controller;
+
+import com.cocotera.interfaces.IOrderRepository;
+import com.cocotera.models.Order;
+import jakarta.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.sql.DataSource;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+@Controller
+public class ReportesController {
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private IOrderRepository orderRepository;
+
+    @GetMapping("/reportes")
+    public void reportes(HttpServletResponse response){
+        response.setHeader("Content-Disposition", "attachment; filename=\"reporte.pdf\";");
+        response.setContentType("application/pdf");
+        try {
+            String ru = resourceLoader.getResource("classpath:static/lazo.jasper").getURI().getPath();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(ru, null, dataSource.getConnection());
+            OutputStream outputStream = response.getOutputStream();
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("filtros")
+    public void reporteConFiltro(HttpServletResponse response, @RequestParam String orderId) {
+        response.setHeader("Content-Disposition", "inline");
+        response.setContentType("application/pdf");
+
+        try {
+            HashMap<String, Object> parametros = new HashMap<>();
+            parametros.put("codigo", orderId);
+            String ru = resourceLoader.getResource("classpath:static/cocotera.jasper").getURI().getPath();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(ru, parametros, dataSource.getConnection());
+            OutputStream outputStream = response.getOutputStream();
+
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
