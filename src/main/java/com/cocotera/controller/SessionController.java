0c7d0e9fc19ebc8a495a8deb493b694344e02238
Heber Lazo <heberlazo@gmail.com>
2024-10-26 11:44:41 -0500

    working user

---
+++ src/main/java/com/cocotera/controller/SessionController.java
@@ -0,0 +1,14 @@
+package com.cocotera.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SessionController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+}
--- src/main/java/com/cocotera/controller/UserController.java
+++ src/main/java/com/cocotera/controller/UserController.java
@@ -1,38 +1,43 @@
 package com.cocotera.controller;
 
 import com.cocotera.interfaces.IUserRepository;
+import com.cocotera.models.Client;
 import com.cocotera.models.User;
 import com.cocotera.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class UserController {
     @Autowired
-    private UserService userService;
+    private IUserRepository userRepository;
 
-    @GetMapping("/register")
-    public String register() {
-        return "register";
+    @GetMapping("/users")
+    public String showClients(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isAuthenticated = authentication != null && authentication.isAuthenticated();
+        model.addAttribute("isAuthenticated", isAuthenticated);
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        model.addAttribute("user", new User());
+        return "users";
     }
 
-    @PostMapping("/register")
-    public String registerUser(@RequestParam String username, @RequestParam String password) {
-        userService.registerUser(username, password);
-        return "redirect:/login";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("/welcome")
-    public String welcome(@AuthenticationPrincipal User user) {
-        return "welcome";
+    @PostMapping("/addUser")
+    public String addClient(@ModelAttribute User user) {
+        user.setUserId(UUID.randomUUID().toString());
+        userRepository.save(user);
+        return "redirect:/users";
     }
 }
